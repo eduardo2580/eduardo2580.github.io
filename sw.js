bf0e0ab47177dac75a833389f870b52c616c8656
eduardo2580 <edu.rodrigues2580@gmail.com>
2026-04-05 15:06:36 -0300

    fix: Error uploading

--- sw.js
+++ sw.js
@@ -2,7 +2,7 @@
    Service Worker – Bíblia Sagrada PWA
    =========================================================== */
 
-const CACHE_NAME    = 'bible-sagrada-v22';
+const CACHE_NAME    = 'bible-sagrada-v23';
 const PRECACHE_NAME = `${CACHE_NAME}-precache`;
 const RUNTIME_NAME  = `${CACHE_NAME}-runtime`;
 
